ec9386fc96a2ddf86306cb60ea2247d78abff1ba
Alex Dinu <adix64@gmail.com>
2020-10-13 22:59:47 +0300

    first commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    float yaw = 0f, pitch = 0f;
+    public Transform player;
+    public float distToTarget = 4f;
+
+    public Vector3 cameraOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X");
+        pitch -= Input.GetAxis("Mouse Y");
+
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
+        transform.position = player.position - transform.forward * distToTarget +
+            transform.TransformVector(cameraOffset);
+    }
+}
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+    Transform camTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");//-1 pentru tasta A, 1 pentru tasta D, 0 altfel
+        float v = Input.GetAxis("Vertical"); //-1 pentru tasta S, 1 pentru tasta W, 0 altfel
+
+        Vector3 moveDir = h * camTransform.right + v * camTransform.forward;
+        moveDir.y = 0f;
+        moveDir = moveDir.normalized;
+        transform.position += moveDir * Time.deltaTime * movementSpeed;
+    }
+}
